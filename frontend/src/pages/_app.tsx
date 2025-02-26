dc6227cb8ea191c69d3fad87a59c9a16d2895ce7
Om Gupta <omgupta0720@gmail.com>
2025-02-26 18:40:53 +0530

    feat: home page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { GeistSans } from "geist/font/sans";
-import { type AppType } from "next/app";
-
+import type { AppType } from "next/app";
+import { Toaster } from "@/components/ui/sonner"
+import { cn } from "@/lib/utils"
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
+    <div className={cn(GeistSans.className, "bg-black min-h-screen text-white")}>
       <Component {...pageProps} />
+      <Toaster />
     </div>
   );
 };
